1848c4bc820946b943063867d9d46c254f01ebc3
Dominick Triola <dominick@ignovadesign.com>
2018-02-25 10:52:58 -0800

    Add express server

---
+++ app/server/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/dist', express.static(path.join(__dirname, '/../../dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(8000, () => {
+  console.log('Listening on port 8000');
+});
